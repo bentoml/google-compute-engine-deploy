b4297268cf5c71c87b1f9052bbe550e18eb61efc
Jithin James <jamesjithin97@gmail.com>
2022-07-06 15:50:56 +0530

    feat: added a wait till service is up (#7)
    * added a wait till service is up
    
    * fix "
    
    * format

--- google_compute_engine/templates/terraform_default.tf
+++ google_compute_engine/templates/terraform_default.tf
@@ -98,6 +98,24 @@ resource "google_compute_instance" "vm" {
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+        attempt_counter=0
+        max_attempts=20
+        printf 'waiting for server to start'
+        until $(curl --output /dev/null --silent --head --fail http://${self.network_interface.0.access_config.0.nat_ip}:3000); do
+            if [ $attempt_counter -eq $max_attempts ];then
+              echo "Max attempts reached"
+              exit 1
+            fi
+
+            printf '.'
+            attempt_counter=$(($attempt_counter+1))
+            sleep 15
+        done
+        EOT
+  }
 }
 
 resource "google_compute_firewall" "default" {
@@ -119,5 +137,5 @@ resource "google_compute_firewall" "default" {
 
 output "endpoint" {
   description = "IP address for the instance"
-  value = "http://${google_compute_instance.vm.network_interface.0.access_config.0.nat_ip}:3000"
+  value       = "http://${google_compute_instance.vm.network_interface.0.access_config.0.nat_ip}:3000"
 }
